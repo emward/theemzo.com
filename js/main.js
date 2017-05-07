a9ca60d9837d380ab1d2787fd53132c6b2013ec4
James Johnson <johnsojr@gmail.com>
2017-05-07 15:41:03 -0500

    basic resposive menu

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $('.js-menu-btn').click(function () {
+    $('.main-nav').toggleClass('is-open');
+  });
+
+  $('.main-nav li').click(function() {
+    // toggle menu closed if menu item is clicked
+    if ($('.main-nav.is-open')) {
+      $('.main-nav').removeClass('is-open');
+    }
+  });
+});
